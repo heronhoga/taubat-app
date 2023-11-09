b687d2be40c5eebb24569b98523b228af38cb39a
heronhoga <nozzeravixer@gmail.com>
2023-11-09 07:39:28 +0700

    feat:adding back button navigating to home from detail page

--- pages/Detail.js
+++ pages/Detail.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Touchable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Detail = ({ route }) => {
   const { doa, ayat, latin, artinya } = route.params; // Get data from navigation parameters
-
+    const { navigate } = useNavigation();
+  
+    const navigateToDoa = () => {
+      navigate("Doa"); // Navigate to the "Detail" component
+    };
   return (
     <View style={styles.container}>
       <Text style={styles.doaTitle}>{doa}</Text>
       <Text style={styles.detailText}>Ayat: {ayat}</Text>
       <Text style={styles.detailText}>Latin: {latin}</Text>
       <Text style={styles.detailText}>Artinya: {artinya}</Text>
+      <TouchableOpacity onPress={navigateToDoa}>
+        <Text>Kembali</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -18,11 +27,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    justifyContent: "center",
+      alignItems: "center", 
   },
   doaTitle: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 16,
+    
   },
   detailText: {
     fontSize: 18,
